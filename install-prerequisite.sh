7e527e4a0fe327e253ce58ce4d4a9453e3f1bd5a
Junhyeok Ahn <junhyeokahn91@gmail.com>
2025-07-04 00:11:19 -0400

    kanagawa

--- install-prerequisite.sh
+++ install-prerequisite.sh
@@ -16,12 +16,6 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
 
     # Set up fzf
     echo 'source <(fzf --zsh)' >> ~/.zshrc
-    echo 'export FZF_DEFAULT_OPTS="
---color=fg:#908caa,bg:#191724,hl:#ebbcba
---color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
---color=border:#403d52,header:#31748f,gutter:#191724
---color=spinner:#f6c177,info:#9ccfd8
---color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"' >> ~/.zshrc
 
 elif [[ "$(uname -s)" == "Linux" ]]; then
     # Update package list and upgrade installed packages
@@ -32,12 +26,6 @@ elif [[ "$(uname -s)" == "Linux" ]]; then
 
     # Set up fzf
     echo 'eval "$(fzf --bash)"' >> ~/.basrc
-    echo 'export FZF_DEFAULT_OPTS="
---color=fg:#908caa,bg:#191724,hl:#ebbcba
---color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
---color=border:#403d52,header:#31748f,gutter:#191724
---color=spinner:#f6c177,info:#9ccfd8
---color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"' >> ~/.bashrc
 
 else
     echo "OS not detected"
